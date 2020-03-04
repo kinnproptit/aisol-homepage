a8417ff8768add452728e245a4c17af4b64cc3c3
Nguyen The Anh <ntheanh201@gmail.com>
2020-03-05 01:23:57 +0700

    fix download button

--- src/app/Shared/components/Button/Button.js
+++ src/app/Shared/components/Button/Button.js
@@ -22,19 +22,23 @@ const Button = ({
     )
   }
 
-  return (
+  return to ? (
+    <a href={to}>
+      <Wrapper
+        className={`btn ${className}`}
+        isMarginLeft={isMarginLeft}
+        onClick={onClick}
+      >
+        <InsideButton />
+      </Wrapper>
+    </a>
+  ) : (
     <Wrapper
       className={`btn ${className}`}
       isMarginLeft={isMarginLeft}
       onClick={onClick}
     >
-      {to ? (
-        <a href={to}>
-          <InsideButton />
-        </a>
-      ) : (
-        <InsideButton />
-      )}
+      <InsideButton />
     </Wrapper>
   )
 }
@@ -45,7 +49,7 @@ const Wrapper = styled.button`
   margin-left: ${props => (props.isMarginLeft ? '15px' : '0px')};
 `
 const Text = styled.p`
-  margin: 0 0 0 10px ;
+  margin: 0 0 0 10px;
   color: white;
   font-size: 16px;
 `
